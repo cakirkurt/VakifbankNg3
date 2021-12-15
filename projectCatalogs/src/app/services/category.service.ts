f438dd90c205424a125085fdddb57b2785b5bceb
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-12-15 10:36:46 +0300

    Add map to pipe

--- projectCatalogs/src/app/services/category.service.ts
+++ projectCatalogs/src/app/services/category.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Category } from '../models/category.model';
 
 @Injectable({
@@ -8,12 +8,18 @@ import { Category } from '../models/category.model';
 })
 export class CategoryService {
 
-  url:string = 'https://mocki.io/v1/e77d1403-4902-4c02-a143-f5a7067eeb94';
-  constructor(private httpClient:HttpClient) { }
+  url: string = 'https://mocki.io/v1/e77d1403-4902-4c02-a143-f5a7067eeb94';
+  constructor(private httpClient: HttpClient) { }
 
 
-  getAllCategories():Observable<Category[]>{
-    return this.httpClient.get<Category[]>(this.url).pipe(tap(data=>console.log(data)));
+  getAllCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.url).pipe(
+      tap(data => console.log(data)),
+      map((data: Category[], index: number) => {
+        let newArray = data.map((value:Category)=> new Category(value.id, "Kategori: " + value.name)) ;
+        return newArray;
+      })
+    );
   }
 
 
